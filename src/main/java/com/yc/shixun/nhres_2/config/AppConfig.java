b5bbdb90536904800b403bda734253560e673bea
zhangyingchengqi <1069595532@qq.com>
2020-06-03 17:28:11 +0800

    加入了spring boot的程序入口

--- src/main/java/com/yc/shixun/nhres_2/config/AppConfig.java
+++ src/main/java/com/yc/shixun/nhres_2/config/AppConfig.java
@@ -27,7 +27,7 @@ public class AppConfig {
 	public DriverManagerDataSource dataSource() {
 		DriverManagerDataSource dataSource = new DriverManagerDataSource();
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
-		dataSource.setUrl("jdbc:mysql://localhost:3306/nhres");
+		dataSource.setUrl("jdbc:mysql://localhost:3306/nhres?serverTimezone=UTC");
 		dataSource.setUsername("root");
 		dataSource.setPassword("a");
 		log.info("创建数据源" + dataSource + "成功");
